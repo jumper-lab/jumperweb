179622a3031b474640a7323920a7cfc8358e218e
Leandro Marajah <leandro@jumper.studio>
2026-07-10 19:26:07 -0300

    Configure Casa Belie sites for Vercel

--- jumper - construtor-de-sites/_jumper-sites-system/clientes/casa-belie/astro.config.mjs
+++ jumper - construtor-de-sites/_jumper-sites-system/clientes/casa-belie/astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://casabelie.com.br',
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   integrations: [sitemap()],
   image: {
     responsiveStyles: true,
